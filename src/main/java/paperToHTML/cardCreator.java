93819ed516bd471b7be69a0c56eb227ce64f0dc7
mtheodorou <marios_theodorou@hotmail.gr>
2020-01-02 13:43:04 +0000

    online database connection test

--- src/main/java/paperToHTML/cardCreator.java
+++ src/main/java/paperToHTML/cardCreator.java
@@ -19,7 +19,16 @@ public class cardCreator {
 
     @Override
     public String toString() {
-        String template = new FileToString("result.html").toString();
+        String template = "\n" +
+                "    <a href=\"LINK\">\n" +
+                "        <div class=\"card w-100 mt-2\">\n" +
+                "            <div class=\"card-body\">\n" +
+                "                <h5 class=\"card-title\">TITLE</h5>\n" +
+                "                <p class=\"card-text\"><strong>AUTHOR</strong></p>\n" +
+                "                <p class=\"card-text\">TEXT</p>\n" +
+                "            </div>\n" +
+                "        </div>\n" +
+                "    </a>";
         template = template.replace("LINK", this.link);
         //template = template.replace("TITLE", "");
         template = template.replace("AUTHOR", this.author);
